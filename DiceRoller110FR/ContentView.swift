a7829435f13515feffae6397d15b7f1a1e710c8e
Jonathan Heinzman <Jond.heinzman@icloud.com>
2026-07-31 16:35:12 -0600

    Initial Commit

---
+++ DiceRoller110FR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DiceRoller110FR
+//
+//  Created by Jonathan Heinzman on 7/31/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DiceRoller110FR/DiceRoller110FRApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRoller110FRApp.swift
+//  DiceRoller110FR
+//
+//  Created by Jonathan Heinzman on 7/31/26.
+//
+
+import SwiftUI
+
+@main
+struct DiceRoller110FRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
